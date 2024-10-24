62453205cec1fa69440dfada38fdbe5f97ae56f9
Jayden Daniello <jcd00013@mix.wvu.edu>
2024-10-24 19:35:33 -0400

    added logo to navbar

---
+++ app/ui/company-logos.tsx
@@ -0,0 +1,10 @@
+import { BeakerIcon } from "@heroicons/react/16/solid";
+
+export default function DefaultLogo() {
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <BeakerIcon className="h-12 w-12" />
+            <p className="text-[12px] mt-2">Your Company Here</p>
+        </div>
+    );
+}
