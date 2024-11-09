13c833091dec6abade8de718c4cffc59b689a803
dps73 <122404372+dps73@users.noreply.github.com>
2024-11-09 23:19:41 +0000

    Added loading skeleton for home page

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -1,10 +1,38 @@
 const shimmer = 
     'before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent';
 
-{ /*
-export function LoginSkeleton() {
+
+export default function HomeSkeleton() {
     return (
-        <div
+        <div className="flex justify-center items-center min-h-screen bg-black-900">
+            <div className="w-full max-w-2xl p-8 bg-black-900 rounded-lg shadow-lg">
+                <div className="animate-pulse space-y-8">
+                    {/* Table Header Skeleton */}
+                    <div className="flex justify-between space-x-4">
+                        <div className="h-6 bg-gray-300 rounded w-1/4"></div>
+                        <div className="h-6 bg-gray-300 rounded w-1/4"></div>
+                        <div className="h-6 bg-gray-300 rounded w-1/4"></div>
+                        <div className="h-6 bg-gray-300 rounded w-1/4"></div>
+                    </div>
+
+                    {/* Table Rows Skeleton */}
+                    {[...Array(10)].map((_, index) => (
+                        <div key={index} className="flex justify-between space-x-8">
+                            <div className="h-6 bg-gray-300 rounded w-1/4"></div>
+                            <div className="h-6 bg-gray-300 rounded w-1/4"></div>
+                            <div className="h-6 bg-gray-300 rounded w-1/4"></div>
+                            <div className="h-6 bg-gray-300 rounded w-1/4"></div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export function LoginSkeleton() {
+        return (
+            <div
         className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm`}
         >
         <div className="flex p-4">
@@ -15,6 +43,6 @@ export function LoginSkeleton() {
             <div className="h-7 w-20 rounded-md bg-gray-200" />
         </div>
         </div>
-    );
+        );
 }
-    */ }
+    
