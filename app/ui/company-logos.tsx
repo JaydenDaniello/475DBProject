96034a924d463f50f5c8637e3fb955927817fc98
dps73 <122404372+dps73@users.noreply.github.com>
2024-12-04 04:39:39 +0000

    Added company name

--- app/ui/company-logos.tsx
+++ app/ui/company-logos.tsx
@@ -4,7 +4,7 @@ export default function DefaultLogo() {
     return (
         <div className="flex flex-col items-center justify-center">
             <BeakerIcon className="h-12 w-12" />
-            <p className="text-[12px] mt-2">Your Company Here</p>
+            <p className="text-[12px] mt-2">Hungry Spiders</p>
         </div>
     );
 }
